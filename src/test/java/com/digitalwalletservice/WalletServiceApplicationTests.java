0fada346648fada10b9d031fb6989c24700e7bc9
Henry García <henrygarciaospina@gmail.com>
2023-08-27 17:13:28 -0500

    Project initial

---
+++ src/test/java/com/digitalwalletservice/WalletServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalwalletservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
